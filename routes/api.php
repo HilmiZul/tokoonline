6839da55dbd1def22d1b36f71d008b11618ec3f7
hilmizul <netspytux@gmail.com>
2022-07-05 21:49:03 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\KategoriApiController;
-use App\Models\Kategori;
+use App\Http\Controllers\ProdukApiController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// API: KATEGORI
 Route::get('/kategori', [KategoriApiController::class, 'getAllKategori']);
 Route::get('/kategori/{kategori}', [KategoriApiController::class, 'getKategori']);
 Route::post('/kategori', [KategoriApiController::class, 'addKategori']);
 Route::put('/kategori/{kategori}', [KategoriApiController::class, 'sunting']);
 Route::delete('/kategori/{kategori}', [KategoriApiController::class, 'hapus']);
+
+// API: PRODUK
+Route::get('/produk', [ProdukApiController::class, 'getAllProduk']);
+Route::get('/produk/{produk}', [ProdukApiController::class, 'getProduk']);
+Route::post('/produk', [ProdukApiController::class, 'addProduk']);
+Route::get('/produk/kategori/{kategori}', [ProdukApiController::class, 'getProdukKat']);
+Route::put('/produk/{produk}', [ProdukApiController::class, 'updateProduk']);
